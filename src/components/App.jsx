29a1af76cb412a015a41edaa4910d1acfa63ef5a
Prashant pal <96548592+Pal2021@users.noreply.github.com>
2023-05-16 04:20:17 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </>
+  );
+}
+export default App;
